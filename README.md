fb6a6c14fae780b4b6f174206dca71886ec0b2b4
Muhammad Albert Nur Agathon <muhammadalbert16@gmail.com>
2023-11-06 22:13:02 +0700

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# reprohealth_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reprohealth_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reprohealth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
